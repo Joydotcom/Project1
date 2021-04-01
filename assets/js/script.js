ec5a253f55162cd28db36f964f789c79e36e4c6e
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-04-01 12:03:29 -0400

    added fetch request we built with Christian and Peter yesterday to console log restaurant API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+// This returns restaurants located bby zip code (currently entered manually)
+fetch("https://api.documenu.com/v2/restaurants/search/fields?zip_code=30308&key=bb830da9585d8d17c53039de5ac3358c")
+.then(response => {
+    return response.json();
+}) .then(function(data){
+    console.log(data.data);
+})
