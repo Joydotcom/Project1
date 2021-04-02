03a9c4af47bc9c578123e6eacc7234432c85d295
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-04-02 12:25:14 -0400

    modifeid HTML link 38 changed if to optionState. modified script.js line 1 var randomChoice. lines 27 though 46 added event listener for button click, ran API to return random activity by state selected and get park name where activity is available. showing in dev tools via console log. Added array of activity options for computer to pick from lines 73 through 81. Added function call random on line 83. Added various notes and console log tests.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,83 @@
+var randomChoice;
 // This event listener on click for foodPageBtn
 $('.foodPageBtn').on('click', function () {
-// This returns restaurants located by zip code (currently entered manually 30308)
- fetch("https://api.documenu.com/v2/restaurants/search/fields?zip_code=30308&key=bb830da9585d8d17c53039de5ac3358c")
- .then(response => {
-    return response.json();
- }) .then(function(data){
-    console.log(data.data);
- })
- });
+    // This returns restaurants located by zip code (currently entered manually 30308)
+     fetch("https://api.documenu.com/v2/restaurants/search/fields?zip_code=30308&key=bb830da9585d8d17c53039de5ac3358c")
+     .then(response => {
+        return response.json();
+     }) .then(function(data){
+        console.log(data.data);
+     })
+     });
+    
+    
+   //  This event listener on click for randomBtn (works for parks?parkCode=acad)
+   //  $('.randomBtn').on('click', function () {
+   //  This returns National Park Activities by State code (currently using sample call)
+   //   fetch("https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=7Ir4CEYWv3mXsoB7yi5AYKGSBLXqPI5lOBOUvet2")
+   //   .then(response => {
+   //       return response.json();
+   //   })  .then(function(data){
+   //       console.log(data.data);
+    
+   //   })    
+   //   });
+    
+    
+    //  This event listener on click for randomBtn
+   $('.randomBtn').on('click', function () {
+console.log($("#optionState").val())
+     // This returns National Parks By Activity 
+     fetch("https://developer.nps.gov/api/v1/activities/parks?id=&q=hiking&limit=5&start=0&api_key=7Ir4CEYWv3mXsoB7yi5AYKGSBLXqPI5lOBOUvet2")
+       .then(response => {
+           return response.json();
+       })  .then(function(data){
+          console.log(data.data[0])
+          for (var i = 0; i < data.data[0].parks.length; i++) {
+             //console.log(data.data[0].parks[i].states);
+             if ($("#optionState").val() === data.data[0].parks[i].states) {
+                  console.log("This is a match for " + data.data[0].parks[i].states + "state")
+                  console.log("This is the park that matches it " + data.data[0].parks[i].fullName)
+          }
+      
+       }    
+       });
+
+    })
+    
+    //Maybe this could be used in a conditional statement that would allow
+    // you to find a park based on state --> data.data[0].parks[0].states
+    
+     // This returns State Park by activity search
 
 
-// This event listener on click for randomBtn IS BROKEN
-// $('.randomBtn').on('click', function () {
-// // This returns National Park Activities by State code (currently using sample call)
-// //  fetch("https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=7Ir4CEYWv3mXsoB7yi5AYKGSBLXqPI5lOBOUvet2")
-// //  .then(response => {
-// //      return response.json();
-// //  })  .then(function(data){
-// //      console.log(data.data);
-
-// //  })    
-//  });
-
-
-
-
- // This returns National Parks By Activity 
 
 
 
 
 
+     // Activity categories and id's
+   //   camping
+   //   hiking
+   //   biking
+   //   fishing
+   //   guided tours
+   //   skiing 
+   //   swimming 
+   //   wildlife watching
 
- // Randomly select activity by state code
+   // create random function with index 0-7
+   // that number will correspond with array of activities 0-7
+
+
+   // Array of options for computer to pick from
+   var activities = ["camping", "hiking", "biking", "fishing", "guided tours", "skiing", "swimming", "wildlife watching"]
+  
+   function random() {
+      var randomChoice = Math.floor(Math.random() * activities.length);
+      console.log(randomChoice);
+      console.log(activities[randomChoice]);
+   }
+   var queryParameter = activities[randomChoice];
+   // when the page loads run random
+   random();
