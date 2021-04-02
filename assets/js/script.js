cc87de617e855933d87cdfd9e5fb4ba7a092213a
Justin Griffin <griffin.justintime@gmail.com>
2021-04-01 20:25:03 -0400

    whatever

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 // This returns restaurants located bby zip code (currently entered manually)
+
+
 fetch("https://api.documenu.com/v2/restaurants/search/fields?zip_code=30308&key=bb830da9585d8d17c53039de5ac3358c")
 .then(response => {
     return response.json();
