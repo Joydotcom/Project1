8024b1a4b5a6573b6c5a0f5de8f3dbfd1805a7a9
joydotcom <msjoyjackson@gmail.com>
2021-04-04 16:14:44 -0400

    testing script files

--- assets/js/activities.js
+++ assets/js/activities.js
@@ -1,4 +1,33 @@
-var activityBtn = document.querySelector("#activities");
+var randomChoice;
+
+//  This event listener on click for randomBtn
+// $(".randomBtn").on("click", function () {
+//   console.log($("#optionState").val());
+  // // This returns National Parks By Activity
+  // fetch(
+  //   "https://developer.nps.gov/api/v1/activities/parks?id=&q=hiking&limit=5&start=0&api_key=7Ir4CEYWv3mXsoB7yi5AYKGSBLXqPI5lOBOUvet2"
+  // )
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then(function (data) {
+  //     console.log(data.data[0]);
+  //     for (var i = 0; i < data.data[0].parks.length; i++) {
+  //       //console.log(data.data[0].parks[i].states);
+  //       if ($("#optionState").val() === data.data[0].parks[i].states) {
+  //         console.log(
+  //           "This is a match for " + data.data[0].parks[i].states + "state"
+  //         );
+  //         console.log(
+  //           "This is the park that matches it " + data.data[0].parks[i].fullName
+  //         );
+  //       }
+  //     }
+  //   });
+
+
+
+// var activityBtn = document.querySelector("#activities");
 var activities = [
   "camping",
   "hiking",
@@ -8,21 +37,23 @@ var activities = [
   "skiing",
   "swimming",
   "wildlife watching",
-];
+]
 
 function random() {
   var randomChoice = Math.floor(Math.random() * activities.length);
-  console.log(randomChoice);
+  // console.log(randomChoice);
   console.log(activities[randomChoice]);
 }
 var queryParameter = activities[randomChoice];
 // when the page loads run random
 random();
 
-activityBtn.addEventListener("click", function (event) {
-  console.log($("#test").val());
-  localStorage.setItem("activity", $("#test").val());
-  event.preventDefault();
-});
+// activityBtn.addEventListener("click", function (event) {
+//   console.log($("#test").val());
+//   localStorage.setItem("activity", $("#test").val());
+//   event.preventDefault();
+// });
 
-localStorage.getItem("activity");
+// localStorage.getItem("activity");
+
+// localStorage.getItem("state");
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-var randomChoice;
+// var randomChoice;
 // This event listener on click for foodPageBtn
-$(".foodPageBtn").on("click", function () {
-  // This returns restaurants located by zip code (currently entered manually 30308)
-  fetch(
-    "https://api.documenu.com/v2/restaurants/search/fields?zip_code=30308&key=bb830da9585d8d17c53039de5ac3358c"
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.data);
-    });
-});
+// $(".foodPageBtn").on("click", function () {
+//   // This returns restaurants located by zip code (currently entered manually 30308)
+//   fetch(
+//     "https://api.documenu.com/v2/restaurants/search/fields?zip_code=30308&key=bb830da9585d8d17c53039de5ac3358c"
+//   )
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data.data);
+//     });
+// });
 
 saveBtn.addEventListener("click", function (event) {
   console.log($("#optionState").val());
@@ -19,7 +19,7 @@ saveBtn.addEventListener("click", function (event) {
   event.preventDefault();
 });
 
-localStorage.getItem("state");
+// localStorage.getItem("state");
 
 //  This event listener on click for randomBtn (works for parks?parkCode=acad)
 //  $('.randomBtn').on('click', function () {
@@ -34,30 +34,30 @@ localStorage.getItem("state");
 //   });
 
 //  This event listener on click for randomBtn
-$(".randomBtn").on("click", function () {
-  console.log($("#optionState").val());
-  // This returns National Parks By Activity
-  fetch(
-    "https://developer.nps.gov/api/v1/activities/parks?id=&q=hiking&limit=5&start=0&api_key=7Ir4CEYWv3mXsoB7yi5AYKGSBLXqPI5lOBOUvet2"
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.data[0]);
-      for (var i = 0; i < data.data[0].parks.length; i++) {
-        //console.log(data.data[0].parks[i].states);
-        if ($("#optionState").val() === data.data[0].parks[i].states) {
-          console.log(
-            "This is a match for " + data.data[0].parks[i].states + "state"
-          );
-          console.log(
-            "This is the park that matches it " + data.data[0].parks[i].fullName
-          );
-        }
-      }
-    });
-});
+// $(".randomBtn").on("click", function () {
+//   console.log($("#optionState").val());
+//   // This returns National Parks By Activity
+//   fetch(
+//     "https://developer.nps.gov/api/v1/activities/parks?id=&q=hiking&limit=5&start=0&api_key=7Ir4CEYWv3mXsoB7yi5AYKGSBLXqPI5lOBOUvet2"
+//   )
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data.data[0]);
+//       for (var i = 0; i < data.data[0].parks.length; i++) {
+//         //console.log(data.data[0].parks[i].states);
+//         if ($("#optionState").val() === data.data[0].parks[i].states) {
+//           console.log(
+//             "This is a match for " + data.data[0].parks[i].states + "state"
+//           );
+//           console.log(
+//             "This is the park that matches it " + data.data[0].parks[i].fullName
+//           );
+//         }
+//       }
+//     });
+// });
 
 //Maybe this could be used in a conditional statement that would allow
 // you to find a park based on state --> data.data[0].parks[0].states
@@ -78,22 +78,22 @@ $(".randomBtn").on("click", function () {
 // that number will correspond with array of activities 0-7
 
 // Array of options for computer to pick from
-var activities = [
-  "camping",
-  "hiking",
-  "biking",
-  "fishing",
-  "guided tours",
-  "skiing",
-  "swimming",
-  "wildlife watching",
-];
+// var activities = [
+//   "camping",
+//   "hiking",
+//   "biking",
+//   "fishing",
+//   "guided tours",
+//   "skiing",
+//   "swimming",
+//   "wildlife watching",
+// ];
 
-function random() {
-  var randomChoice = Math.floor(Math.random() * activities.length);
-  console.log(randomChoice);
-  console.log(activities[randomChoice]);
-}
-var queryParameter = activities[randomChoice];
-// when the page loads run random
-random();
+// function random() {
+//   var randomChoice = Math.floor(Math.random() * activities.length);
+//   console.log(randomChoice);
+//   console.log(activities[randomChoice]);
+// }
+// var queryParameter = activities[randomChoice];
+// // when the page loads run random
+// random();
