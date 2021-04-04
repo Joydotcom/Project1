6921502091a7c33e40648507f8f370bcb1677021
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-04-04 17:44:48 -0400

    repaired activity.js code

--- assets/js/activities.js
+++ assets/js/activities.js
@@ -1,33 +1,3 @@
-var randomChoice;
-
-//  This event listener on click for randomBtn
-// $(".randomBtn").on("click", function () {
-//   console.log($("#optionState").val());
-  // // This returns National Parks By Activity
-  // fetch(
-  //   "https://developer.nps.gov/api/v1/activities/parks?id=&q=hiking&limit=5&start=0&api_key=7Ir4CEYWv3mXsoB7yi5AYKGSBLXqPI5lOBOUvet2"
-  // )
-  //   .then((response) => {
-  //     return response.json();
-  //   })
-  //   .then(function (data) {
-  //     console.log(data.data[0]);
-  //     for (var i = 0; i < data.data[0].parks.length; i++) {
-  //       //console.log(data.data[0].parks[i].states);
-  //       if ($("#optionState").val() === data.data[0].parks[i].states) {
-  //         console.log(
-  //           "This is a match for " + data.data[0].parks[i].states + "state"
-  //         );
-  //         console.log(
-  //           "This is the park that matches it " + data.data[0].parks[i].fullName
-  //         );
-  //       }
-  //     }
-  //   });
-
-
-
-// var activityBtn = document.querySelector("#activities");
 var activities = [
   "camping",
   "hiking",
@@ -37,16 +7,57 @@ var activities = [
   "skiing",
   "swimming",
   "wildlife watching",
-]
-
-function random() {
-  var randomChoice = Math.floor(Math.random() * activities.length);
-  console.log(randomChoice);
-  console.log(activities[randomChoice]);
+];
+function getRandomActivity() {
+  var randomNumber = Math.floor(Math.random() * activities.length);
+  // console.log(randomChoice);
+  // console.log(activities[randomNumber]);
+  return activities[randomNumber];
 }
-var queryParameter = activities[randomChoice];
-// when the page loads run random
-random();
+function displayActivityData(activity) {
+  console.log(activity);
+  // fetch and display data
+  // get State, build url with state and activity
+}
+
+var randomActivity = getRandomActivity();
+displayActivityData(randomActivity);
+
+//  This event listener on click for randomBtn
+// $(".randomBtn").on("click", function () {
+//   console.log($("#optionState").val());
+// This returns National Parks By Activity
+// fetch(
+//   "https://developer.nps.gov/api/v1/activities/parks?id=&q=(activity)&limit=5&start=0&api_key=7Ir4CEYWv3mXsoB7yi5AYKGSBLXqPI5lOBOUvet2"
+// )
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data.data[0]);
+//     for (var i = 0; i < data.data[0].parks.length; i++) {
+//       //console.log(data.data[0].parks[i].states);
+//       if ($("#optionState").val() === data.data[0].parks[i].states) {
+//         console.log(
+//           "This is a match for " + data.data[0].parks[i].states + "state"
+//         );
+//         console.log(
+//           "This is the park that matches it " + data.data[0].parks[i].fullName
+//         );
+//       }
+//     }
+//   });
+//get the state from local storage
+//get a random activity
+// ar randomActivity = getRandomActivity()v
+//build url and put it on the page
+
+// var activityBtn = document.querySelector("#activities");
+
+// document.appendChild;
+// // when the page loads run random
+// random();
+// var queryParameter = activities[randomChoice];
 
 // activityBtn.addEventListener("click", function (event) {
 //   console.log($("#test").val());
